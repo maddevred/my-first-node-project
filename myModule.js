12d55d1a74e043ebffc7a598ca2b4b931b52d909
Amari James SEI <73178321+marjames98@users.noreply.github.com>
2020-11-12 15:09:45 -0500

    Create myModule.js

---
+++ myModule.js
@@ -0,0 +1,20 @@
+const beBasic = () => "That's so fetch!"
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num2 - num1;
+}
+
+function addAgain(num1, num2) {
+    return num1 + num2;
+}
+
+module.exports = {
+    beBasic,
+    add,
+    subtract, 
+    addAgain
+}
