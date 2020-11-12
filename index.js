5cf6033e7c668ada5865dbef0f064d9ac3be83af
Amari James SEI <73178321+marjames98@users.noreply.github.com>
2020-11-12 15:10:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-let name = "Amari James"
+const {add, subtract, addAgain }= require('./myModule');
+
+let name = 'Amari James'
 console.log(name);
 
 function printName(person) {
-    return 'Hello, ${person}';
+    return `Hello, ${person}`;
 }
 
 console.log(printName(name));
+
+console.log(add(5, 50));
+console.log(subtract(10, 20));
+console.log(addAgain(100, 342));
